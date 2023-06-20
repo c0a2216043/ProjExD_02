c9bbb8f20cb0379cf97f273fdce953e24587eaa4
kosugi <C0A2216043@edu.teu.ac.jp>
2023-06-20 14:23:39 +0900

    練習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,7 +4,6 @@ import random
 
 WIDTH, HEIGHT = 1000, 600
 
-
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -16,21 +15,24 @@ def main():
     bd_img.set_colorkey((0, 0, 0))
     clock = pg.time.Clock()
     tmr = 0
+    
     x = random.randint(0,WIDTH)
     y = random.randint(0,HEIGHT)
     bd_rct = bd_img.get_rect()
     bd_rct.center = x, y
+    vx, vy = 5, 5 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
 
+        bd_rct.move_ip(vx, vy)
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
         screen.blit(bd_img,bd_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
@@ -38,3 +40,4 @@ if __name__ == "__main__":
     main()
     pg.quit()
     sys.exit()
+    
